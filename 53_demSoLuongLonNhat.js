49003f95c00afc3e135b9f8cb9927c032c045ac5
unknown <nguyentiendoanhn0@gmail.com>
2020-10-28 16:49:06 +0700

    chua fix

--- 53_demSoLuongLonNhat.js
+++ 53_demSoLuongLonNhat.js
@@ -1,3 +1,30 @@
-function demSoLuongChuSO(){
-    
+
+
+function demSoLuongChuSOmax(n){
+var max=0;
+while(n>0)
+{
+    if(n%10 > max )
+    {
+        max=n%10;
+    }
+    n=Math.floor(n/10);
 }
+return max;
+}
+
+ function demmax(m)
+{
+    var m=n;
+    var dem=0;
+    while(n>0)
+    {
+        if(n%10==demso)
+        {
+            dem++;
+        }
+        n=Math.floor(n/10);
+    }
+    return demmax;
+}
+console.log("Số lần xuất hiện số lớn nhất của số nguyên dương n là:"+demmax(3456777));
