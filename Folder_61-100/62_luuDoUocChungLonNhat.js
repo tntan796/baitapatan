eefab74e3e807227b4adbdd33893d8b44376a006
DoanNT <nguyentiendoanhn09@gmail.com>
2020-10-31 21:03:31 +0700

    update 2 file ve luu do uoc chung lon nhat

---
+++ Folder_61-100/62_luuDoUocChungLonNhat.js
@@ -0,0 +1,16 @@
+function luuDoUCLN(a,b){
+  
+    while(a!=b)
+    {
+        if(a>b)
+        {
+            a=a-b;
+        }
+        else
+        {
+            b=b-a;
+        }
+    }
+    return a;
+}
+console.log("Ước chung lớn nhất là "+luuDoUCLN(6,8))
