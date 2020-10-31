68f52b0d9a20cc7a122d8bac3a0c76eba93fcffe
unknown <nguyentiendoanhn0@gmail.com>
2020-10-31 11:47:45 +0700

    update bai 60

---
+++ Folder_43-48/60_kiemtraTraiQuaPhai.js
@@ -0,0 +1,18 @@
+function kiemTraTraiQuaPhai(n){
+    var TangDan=n%10;
+    n=Math.floor(n/10);
+    var kiemtra="";
+    while(n>0)
+    {
+          kiemtra=n%10;
+        if(kiemtra<=TangDan)
+        {
+            return true;
+            
+        }
+        n=Math.floor(n/10);
+    }
+    return false;
+    
+}
+console.log("In ra kết quả "+kiemTraTraiQuaPhai(4321))
