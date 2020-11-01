97e1655d1d52a363c4c097f6c620a2cd7429b2ac
DoanNT <nguyentiendoanhn09@gmail.com>
2020-11-01 21:52:00 +0700

    update giai phuong trinh bac 2

--- Folder_61-100/65_giaiPTB2.js
+++ Folder_61-100/65_giaiPTB2.js
@@ -1,9 +1,6 @@
 function giaiPTB2(a,b,c)
-{   
-     var x= -b/2*a;
-    var delta=(b**b)-(4*a*c);
-    var x1=(-b+Math.sqrt(delta))/2*a;
-    var x2=(-b-Math.sqrt(delta))/2*a;
+{  
+    var delta=(b*b)-(4*a*c);
     
     if(a==0)// trở thành phương trình bâc 
     {
@@ -28,13 +25,13 @@ function giaiPTB2(a,b,c)
             console.log("Phương trình vô nghiệm ");
         }else if(delta==0)
         {
-            console.log("Phương trình có nghiệm kép x="+x);
+            console.log("Phương trình có nghiệm kép x="+-b/2*a);
         }
         else
         {
-            console.log("Phương trình có 2 nghiệm x1="+x1+"và x2="+x2);
+            console.log("Phương trình có 2 nghiệm x1="+(-b+Math.sqrt(delta))/(2*a)+"và x2="+(-b-Math.sqrt(delta))/(2*a));
         }
     }
-    return x1,x2;
+    
 }
-giaiPTB2(1,3,-4);
+giaiPTB2(1,-3,2);
