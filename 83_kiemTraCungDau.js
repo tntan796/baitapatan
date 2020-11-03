65b4c58785c79a3a97ac38357fff51fc4cdbad83
unknown <nguyentiendoanhn0@gmail.com>
2020-11-03 11:09:41 +0700

    update 2 file kiêm tra 2 so vùa nhap co cung dau hay không

---
+++ 83_kiemTraCungDau.js
@@ -0,0 +1,13 @@
+function kiemtracungdau(a,b){
+   
+    var ketqua=a*b;
+    if(ketqua > 0)
+    {
+        console.log("2 số vừa nhập là cùng dấu");
+    }
+    else
+    {
+        console.log("2 số vừa nhập là trái dấu")
+    }
+}
+kiemtracungdau(2,-2);
