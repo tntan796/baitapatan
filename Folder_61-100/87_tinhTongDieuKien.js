33414c87258f4feb097f19fa435648eca9db7939
DoanNT <nguyentiendoanhn09@gmail.com>
2020-11-04 22:27:53 +0700

    update bai 88 in tu a den z

--- Folder_61-100/87_tinhTongDieuKien.js
+++ Folder_61-100/87_tinhTongDieuKien.js
@@ -1,17 +1,8 @@
-function tinhTong87(n)
-{
+
     var Ketqua=1;
     var tong =0;
-    for(var i=1;i<10000;i++)
+    for(var i=1;i<10;i++)
     {
-       
-        tong=tong+i; 
     }
-    if(tong>10000)
-    {
-        Ketqua=Ketqua*i;
-    }
-    return Ketqua;
-
-}
+    
 console.log("Kết quả n thỏa mãn là "+Ketqua);
---
+++ Folder_61-100/88_intuAdenZ.js
@@ -0,0 +1,7 @@
+function inraadenz(n){
+    for(var i=65;i<=n;i++)
+    {
+        console.log(String.fromCharCode(i));
+    }
+}
+inraadenz(90);
