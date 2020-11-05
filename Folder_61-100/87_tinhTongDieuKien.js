4bf1d433ed4d0db4c7c8caba10dc2836c8ffd363
unknown <nguyentiendoanhn0@gmail.com>
2020-11-05 14:29:39 +0700

    update

--- Folder_61-100/87_tinhTongDieuKien.js
+++ Folder_61-100/87_tinhTongDieuKien.js
@@ -3,12 +3,12 @@
 
     var Ketqua=1;
     var tong =0;
-    for(var i=1;i<10;i++)
+    for(var i=1;i<10000;i++)
     {
 
       tong=tong+i;
       Ketqua=i;
-      if(tong>10)
+      if(tong>10000)
       {
           break;
       }
